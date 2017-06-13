b1d11e24222ad9b589dcc842c8d5f9a970a481fc
Dimas J. Taniawan <dimazniawan@gmail.com>
2017-06-14 05:11:14 +0700

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   root "pages#index"
 
-  # devise_for :users, controllers: {
-  #   registrations: "api/users/registrations",
-  #   sessions: "api/users/sessions"
-  # }
-
-  devise_scope :user do
-    post "/api/login", to: "api/users/sessions#create"
-    post "/api/register", to: "api/users/registrations#create"
-  end
+  devise_for :users, only: []
 
   namespace :api, defaults: { format: :json } do
+    devise_scope :user do
+      post "register", to: "users/registrations#create"
+      post "login", to: "users/sessions#create"
+    end
+
     namespace :v1 do
     end
   end
