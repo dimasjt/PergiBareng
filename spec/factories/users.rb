6d2f9b5b66cdbf278c5729dc7276a47c9019ca26
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-06-14 14:22:18 +0700

    login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password { "secr33t()" }
+    confirmed_at Time.now
   end
 
   factory :user_credential, class: User do
