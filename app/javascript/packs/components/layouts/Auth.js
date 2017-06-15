cb8a2567a0dbc9de7ac3a89cb6f437d15cbddbf9
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-06-15 11:07:07 +0700

    handle state login and register

--- app/javascript/packs/components/layouts/Auth.js
+++ app/javascript/packs/components/layouts/Auth.js
@@ -17,18 +17,50 @@ const styles = {
 };
 
 export default class Auth extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      register: {
+        email: null,
+        password: null,
+      },
+      login: {
+        email: null,
+        password: null,
+      },
+    };
+  }
+  handleChange = (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+    let type = event.target.form.name;
+
+    this.setState({
+      [type]: {
+        ...this.state[type],
+        [name]: value,
+      }
+    });
+  }
   render() {
     return (
       <div style={styles.container}>
         <Paper zDepth={1} style={styles.wrapper}>
-          <form>
+          <form name="register">
             <TextField
               hintText="Email"
               fullWidth
+              type="email"
+              name="email"
+              onChange={this.handleChange}
             />
             <TextField
               hintText="Password"
               fullWidth
+              type="password"
+              name="password"
+              onChange={this.handleChange}
             />
             <br />
             <Button label="Login" primary />
@@ -36,14 +68,20 @@ export default class Auth extends Component {
         </Paper>
 
         <Paper zDepth={1} style={styles.wrapper}>
-          <form>
+          <form name="login">
             <TextField
               hintText="Email"
               fullWidth
+              type="email"
+              name="email"
+              onChange={this.handleChange}
             />
             <TextField
               hintText="Password"
               fullWidth
+              type="password"
+              name="password"
+              onChange={this.handleChange}
             />
             <Button label="Register" primary />
           </form>
--- app/javascript/packs/reducers/index.js
+++ app/javascript/packs/reducers/index.js
@@ -2,7 +2,12 @@ import { combineReducers } from "redux";
 import { routerReducer as routing } from "react-router-redux";
 
 import places from "./places";
+import user from "./user";
 
-const combinedReducers = combineReducers({ places, routing });
+const combinedReducers = combineReducers({
+  places,
+  user,
+  routing,
+});
 
 export default combinedReducers;
