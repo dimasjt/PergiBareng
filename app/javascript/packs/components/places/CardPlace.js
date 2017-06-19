d332ccb0e96dcb92964bb270f74c783099517577
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-06-19 10:32:39 +0700

    change to pure func

--- app/javascript/packs/components/places/CardPlace.js
+++ app/javascript/packs/components/places/CardPlace.js
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from "material-ui/Card";
+import { Card, CardMedia, CardTitle } from "material-ui/Card";
 import { Link } from "react-router-dom";
 
-export default class CardPlace extends Component {
-  render() {
-    const place = this.props.place;
+const CardPlace = ({ place }) => {
+  const overlay = <CardTitle title={place.name} />;
 
-    const overlay = <CardTitle title={place.name} />;
+  return (
+    <Link to={`/places/${place.slug}`}>
+      <Card>
+        <CardMedia overlay={overlay}>
+          <img src={place.image.medium} alt={place.name} />
+        </CardMedia>
+      </Card>
+    </Link>
+  );
+};
 
-    return (
-      <Link to={`/places/${place.slug}`}>
-        <Card>
-          <CardMedia overlay={overlay}>
-            <img src={place.image.medium} />
-          </CardMedia>
-        </Card>
-      </Link>
-    );
-  }
-}
+CardPlace.propTypes = {
+  place: PropTypes.object.isRequired,
+};
+
+export default CardPlace;
