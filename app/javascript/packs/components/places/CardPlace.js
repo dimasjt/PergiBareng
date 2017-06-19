1140fafa77578171b24e17e0b5349fc9f8c12c14
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-06-19 10:26:43 +0700

    add link to detail page

--- app/javascript/packs/components/places/CardPlace.js
+++ app/javascript/packs/components/places/CardPlace.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from "material-ui/Card";
+import { Link } from "react-router-dom";
 
 export default class CardPlace extends Component {
   render() {
     const place = this.props.place;
 
-    const overlay = <CardTitle title={place.name} />
+    const overlay = <CardTitle title={place.name} />;
 
     return (
-      <Card>
-        <CardMedia overlay={overlay}>
-          <img src={place.image.medium} />
-        </CardMedia>
-      </Card>
-    )
+      <Link to={`/places/${place.slug}`}>
+        <Card>
+          <CardMedia overlay={overlay}>
+            <img src={place.image.medium} />
+          </CardMedia>
+        </Card>
+      </Link>
+    );
   }
 }
