892833f94800275251508bfdcaa9ad1808c3038f
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-06-21 15:50:51 +0700

    add profile init

--- app/javascript/packs/components/layouts/Profile.js
+++ app/javascript/packs/components/layouts/Profile.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { GridList, GridTile } from "material-ui/GridList";
+import { Paper } from "material-ui";
 
-export default class Profile extends Component {
+// const LeftProfile = (props) => {
+//
+// }
+
+class Profile extends Component {
   render() {
     return (
-      <div>Profile</div>
+      <div>
+        <GridList
+          cellHeight="auto"
+          cols={3}
+        >
+          <GridTile cols={1}>
+            <Paper zDepth={1}>
+            </Paper>
+          </GridTile>
+
+          <GridTile cols={2}>
+            <Paper zDepth={1}>
+            </Paper>
+          </GridTile>
+        </GridList>
+      </div>
     );
   }
 }
+
+export default connect(
+  state => state,
+)(Profile);
