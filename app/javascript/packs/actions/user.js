be311068f5801b07b478d603aedc297a6300f4fc
Dimas J. Taniawan <dimazniawan@gmail.com>
2017-07-08 19:06:16 +0700

    only filled params

--- app/javascript/packs/actions/user.js
+++ app/javascript/packs/actions/user.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 
 import {
   USER_UPDATED,
+  SHOW_FLASH,
 } from "../constants";
 import { setAuthToken } from "../utils/auth";
 import { AxioDevise } from "../Axio";
@@ -16,13 +17,20 @@ export function updateUser(user) {
   return async (dispatch) => {
     try {
       const formData = new FormData();
-      console.log('user', user)
-      _.each(user, (val, key) => formData.append(`user[${key}]`, val));
+      _.each(user, (val, key) => {
+        if (((val !== undefined || val !== "") && key !== "avatar") || (key === "avatar" && val.constructor.name === "File")) {
+          formData.append(`user[${key}]`, val);
+        }
+      });
       const result = await AxioDevise.put("/profile", formData, config);
       const userAttribute = setAuthToken(result.data.auth_token);
       dispatch({
         type: USER_UPDATED,
         user: userAttribute,
+      });
+      dispatch({
+        type: SHOW_FLASH,
+        flash: result.data.flash,
       });
     } catch (error) {
       console.log(error)
