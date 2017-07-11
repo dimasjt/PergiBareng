25a4acd32e564933bc218ac14bd79b365d246494
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-11 13:42:46 +0700

    add specs

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:address) }
 end
