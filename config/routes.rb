25405b722dd48c621e627051091ea9f1f9db056e
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-13 14:15:36 +0700

    remove old API

--- config/routes.rb
+++ config/routes.rb
@@ -9,32 +9,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     post "/graphql", to: "graphql#execute"
-
-    devise_scope :user do
-      post "register", to: "users/registrations#create"
-      match "profile", to: "users/registrations#update", via: %i[put patch]
-      post "login", to: "users/sessions#create"
-      get "confirmations", to: "users/confirmations#show"
-    end
-
-    namespace :v1 do
-      resources :places, except: [:new, :edit] do
-        resources :schedules, only: [:create, :index] do
-          member do
-            post :join
-            delete :unjoin
-          end
-        end
-      end
-
-      resources :users, only: [] do
-        collection do
-          get :schedules
-          get :user_schedules
-          get :places
-        end
-      end
-    end
   end
 
   get "*path", to: "pages#index"
