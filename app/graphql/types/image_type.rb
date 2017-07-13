89255f662e291412c2cc1563aff07e49049d3290
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-13 16:10:15 +0700

    show image

---
+++ app/graphql/types/image_type.rb
@@ -0,0 +1,19 @@
+Types::ImageType = GraphQL::ObjectType.define do
+  name "ImageType"
+
+  field :thumb, types.String do
+    resolve ->(obj, args, ctx) { obj.thumb.url }
+  end
+
+  field :medium, types.String do
+    resolve ->(obj, args, ctx) { obj.medium.url }
+  end
+
+  field :large, types.String do
+    resolve ->(obj, args, ctx) { obj.large.url }
+  end
+
+  field :original, types.String do
+    resolve ->(obj, args, ctx) { obj.url }
+  end
+end
--- app/graphql/types/place_type.rb
+++ app/graphql/types/place_type.rb
@@ -1,5 +1,12 @@
 Types::PlaceType = GraphQL::ObjectType.define do
   name "Place"
+
+  field :id, types.ID
   field :name, types.String
   field :description, types.String
+  field :address, types.String
+  field :slug, types.String
+  field :latitude, types.Int
+  field :longitude, types.Int
+  field :image, Types::ImageType
 end
